39c4b656d2155d0991b78613a6154b70e245d74e
Joshua Guinness <joshua.guinness@gmail.com>
2019-02-17 03:10:04 -0500

    Added deletion and addition functionality for courses on the user profile screen

--- lib/profile.dart
+++ lib/profile.dart
@@ -2,9 +2,98 @@
 
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
+class ProfileState extends State<Profile> {
+
+  //final Set<String> listOfClasses = new Set<String>();
+  final List<String> listOfClasses = <String>['2AA4',
+  '2GA3', '2FA3', '2XB3', '2CO3'];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+  final myController = TextEditingController();
+
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(centerTitle: true, title: Text('User Profile')));
+        resizeToAvoidBottomPadding: false,
+        appBar: AppBar(centerTitle: true, title: Text('User Name')),
+        body: Column(
+          children: <Widget>[
+            Text('Below are listed the classes that you are a part of'),
+            Container(
+                margin: const EdgeInsets.all(10.0),
+                height: 100.0,
+                child: _buildClasses()
+            ),
+            Text('If you would like to add a class, type it below'
+                'then click the add button.'),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Please enter your course code e.g. 2aa4',
+                      ),
+                      controller: myController
+                  )
+                ),
+                Expanded(
+                    child: IconButton(icon: const Icon(Icons.add_box),
+                        onPressed: (){
+                          setState(() {
+                            listOfClasses.add(myController.text);
+                          });
+                        },
+                    )
+                )
+              ],
+            ),
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain, // otherwise the logo will be tiny
+                child: const FlutterLogo(),
+              ),
+            ),
+          ],
+        )
+    );
   }
+
+  Widget _buildClasses(){
+    return new ListView.separated(
+        padding: const EdgeInsets.all(25),
+        itemCount: listOfClasses.length,
+        separatorBuilder: (context, index) => Divider(
+          height: 0.0,
+          color: Colors.white70,
+        ),
+        itemBuilder: (BuildContext _context, int index){
+          if (index < listOfClasses.length){
+            return _buildClass(listOfClasses[index], index);
+          }
+        }
+    );
+  }
+
+  Widget _buildClass(String classes, int index){
+    return ListTile(
+        title: Center(
+            child: Text(
+              classes,
+              style: _biggerFont,
+            )
+        ),
+        trailing: new Icon(
+          Icons.delete
+        ),
+        onTap: (){
+          setState((){
+            listOfClasses.removeAt(index);
+          });
+        }
+    );
+  }
+}
+
+class Profile extends StatefulWidget {
+  @override
+  ProfileState createState() => new ProfileState();
 }
