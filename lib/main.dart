a7cb6b261807ff14bcbc21aa4b7379d57bbb6e5b
[Evan Kim] <[em2kim@uwaterloo.ca]>
2019-02-17 06:37:45 -0500

    you already know

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,12 @@ import 'package:flutter/material.dart';
 import 'new-study-session.dart';
 import 'profile.dart';
 import 'study-session-details.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
 
 void main() => runApp(MyApp());
-
-class Session {
-  String theClass;
-  String sessionTitle;
-  String theBuilding;
-  int groupEndorsement;
-  int startTime, endTime;
-}
 
 class MyApp extends StatelessWidget {
   @override
@@ -24,28 +20,77 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Session {
+
+  final int id;
+  final String start_time;
+  final String end_time;
+  final String building;
+  final String courses;
+
+  Session({this.id, this.start_time, this.end_time,
+     this.building, this.courses});
+
+  factory Session.fromJson(Map<String, dynamic> json) {
+    return Session(
+      id: json['id'],
+      start_time: json['start_time'],
+      end_time: json['end_time'],
+      building: json['building'],
+      courses: json['courses']
+    );
+
+  }
+}
+
+Future<List<Session>> fetchPost() async {
+  http.Response resp = await http.get('http://studyup.appspot.com/getsessions');
+
+  // Decoding the Response Body and putting it in an array
+  List l = json.decode(resp.body);
+  List<Session> sessions = new List();
+  l.forEach((i) {
+    sessions.add(new Session.fromJson(i));
+    return i;
+  });
+  for (int j = 0; j < sessions.length; j++) {
+    print (j);
+    print (sessions[j]);
+  }
+  return sessions;
+  // Parse through sessions (individual JSON Objects)
+  // Pass them into .fromJson
+  // Get an array of session objects
+  // Return this array of session objects
+
+  /*
+  if (resp.statusCode == 200) {
+    // If server returns an OK response, parse the JSON
+    return sessions.fromJson(json.decode(resp.body));
+  } else {
+    // If that response was not OK, throw an error.
+    throw Exception('Failed to load post');
+  }
+  */
+
+}
+
 class StudySessionsState extends State<StudySessions> {
+
+  Future<List<Session>> post;
+  @override
+  void initState() {
+    super.initState();
+    post = fetchPost();
+  }
+
   final List<String> studyGroups = <String>[
     'A',
     'B',
-    'C',
-    'D',
-    'B',
-    'C',
-    'D',
-    'B',
-    'C',
-    'D',
-    'B',
-    'C',
-    'D',
-    'B',
-    'C',
-    'D'
+    'C'
   ];
 
   Widget build(BuildContext context) {
-    print('testing 1, 2, 3');
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Colors.black,
